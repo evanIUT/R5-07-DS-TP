36dc6bc71a95ea596b7edc7f72db68dc7c645ccb
evanIUT <evancnaepelnickx@gmail.com>
2025-10-08 16:41:18 +0200

    Update Q5.java

--- Q5.java
+++ Q5.java
@@ -1,13 +1,13 @@
 public class Q5 {
     public static void main(String[] args) {
         if (args.length != 1) {
-            System.out.println("Usage: java Q4 <N>");
+            System.out.println("Usage: java Q5 <N>");
             return;
         }
         int N = Integer.parseInt(args[0]);
         for (int i = N; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j + (j == i ? "" : " "));
+                System.out.print("x" + (j == i ? "" : " "));
             }
             System.out.println();
         }
